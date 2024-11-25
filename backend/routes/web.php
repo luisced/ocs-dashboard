3df54f121fc7ff9ea29b976971ed677e507af13f
luisced <luis.cedillo.maldonado@gmail.com>
2024-11-24 19:36:37 -0600

    feat (front/components): Add user component structure

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HealthCheckController;
 use App\Http\Controllers\Projects;
+use App\Http\Controllers\Users;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,3 +17,6 @@ Route::post('/projects', [Projects::class, 'store']);
 Route::get('/projects/{project}', [Projects::class, 'show']);
 Route::put('/projects/{project}', [Projects::class, 'update']);
 Route::delete('/projects/{project}', [Projects::class, 'destroy']);
+
+// Users route
+Route::get('/users', [Users::class, 'index']);
