534ad55f984dd71e71c9674f5ef14d80ec05d110
luisced <luis.cedillo.maldonado@gmail.com>
2024-11-24 23:15:27 -0600

    feat: Angular project call

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-
+import { ProjectsComponent } from './projects.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-
+  { path: 'projects', component: ProjectsComponent },
 ];
