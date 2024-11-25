7b442684f884b10e7c8100629a39d81ea7f723ce
GerardoHuHer <gerardohuerta1502@gmail.com>
2024-11-25 17:08:59 -0600

    Se añaden componente del formulario y del dashboard de admin

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectListComponent } from './components/project-list/project-list.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { AdminFormComponent } from './components/admin-form/admin-form.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectListComponent },
+  { path: 'adminDasboard', component: AdminDashboardComponent },
+  { path: 'adminProject', component: AdminFormComponent },
+
 ];
