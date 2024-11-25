466a3a2b400b331e52599fa743547dbc03178146
luisced <luis.cedillo.maldonado@gmail.com>
2024-11-24 19:05:31 -0600

    feat: Add seeders

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,11 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HealthCheckController;
-
+use App\Http\Controllers\Projects;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/health', [HealthCheckController::class, 'check']);
+
+// Projects CRUD routes
+Route::get('/projects', [Projects::class, 'index']);
+Route::post('/projects', [Projects::class, 'store']);
+Route::get('/projects/{project}', [Projects::class, 'show']);
+Route::put('/projects/{project}', [Projects::class, 'update']);
+Route::delete('/projects/{project}', [Projects::class, 'destroy']);
